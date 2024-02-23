68ced60afd4c2caa551b80efc2de67aecf8e9253
Dmitrii Rashchenko <dimitree54@gmail.com>
2024-02-23 20:18:38 +0200

    base classes

--- ai_code_reviewer/base.py
+++ ai_code_reviewer/base.py
@@ -1,29 +1,75 @@
 from abc import ABC
-from typing import List
+from typing import List, Dict
 
-from pydantic import BaseModel
+from langchain import hub
+from langchain.output_parsers import PydanticOutputParser
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.messages import BaseMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableSerializable
+from pydantic import BaseModel, Field
+from yid_langchain_extensions.utils import pydantic_v1_port
 
 
-class Review(BaseModel):
-    line_number: int
+class FilePatchComment(BaseModel):
+    line_number: int  # enumeration starts from first hunk
     comment: str
 
 
+class FilePatchReview(BaseModel):
+    comments: List[FilePatchComment]
+
+    @property
+    def approve(self) -> bool:
+        return len(self.comments) == 0
+
+
 class Reviewer(BaseModel, ABC):
-    def review_file_changes(self, hunk_with_line_numbers: str) -> List[Review]:
+    async def review_file_patch(self, patch: str) -> FilePatchReview:
         pass
 
 
-class FakeReviewer(Reviewer):
-    def review_file_changes(self, file_changes: str) -> List[Review]:
-        return []
+class ProgrammingPrinciple(BaseModel):
+    name: str = Field(alias="principle_name")
+    description: str = Field(alias="principle_description")
+    review_required_examples: str
+    review_not_required_examples: str
 
 
-class TestReviewer(Reviewer):
-    def review_file_changes(self, file_changes: str) -> List[Review]:
-        return [
-            Review(
-                line_number=3,
-                comment="test_review"
-            )
-        ]
+def build_patch_review_chain() -> RunnableSerializable[Dict, FilePatchReview]:
+    llm = ChatOpenAI(
+        model_name="gpt-4-0125-preview",
+        temperature=0
+    )
+    output_parser = PydanticOutputParser(pydantic_object=FilePatchReview)
+
+    principle_checking_template: ChatPromptTemplate = hub.pull("dimitree54/programming_principle_template")
+    principle_checking_template = principle_checking_template.partial(
+        format_instructions=output_parser.get_format_instructions()
+    )
+    return principle_checking_template | llm | output_parser
+
+
+class ProgrammingPrincipleChecker(Reviewer):
+    programming_principle: ProgrammingPrinciple
+    patch_review_chain: pydantic_v1_port(RunnableSerializable[List[BaseMessage], FilePatchReview]) = Field(
+        default_factory=build_patch_review_chain
+    )
+
+    def _add_line_numbers_to_patch(self, patch: str) -> str:
+        result = ""
+        for line_number, line in enumerate(patch):
+            result += f"{line_number}: {line}\n"
+        return result
+
+    async def review_file_patch(self, patch: str) -> FilePatchReview:
+        enumerated_patch = self._add_line_numbers_to_patch(patch)
+        messages = self.principle_checking_template.format_messages(
+            enumerated_patch=enumerated_patch,
+            principle_name=self.programming_principle.name,
+            principle_description=self.programming_principle.description,
+            review_required_examples=self.programming_principle.review_required_examples,
+            review_not_required_examples=self.programming_principle.review_not_required_examples,
+        )
+        file_patch_review: FilePatchReview = await self.patch_review_chain.ainvoke(messages)
+        return file_patch_review
