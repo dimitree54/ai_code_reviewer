c689035275250162652a0818404f941da570189e
Dmitrii Rashchenko <dimitree54@gmail.com>
2024-02-24 20:11:26 +0200

    log time and cost

--- ai_code_reviewer/cli.py
+++ ai_code_reviewer/cli.py
@@ -2,18 +2,20 @@ import argparse
 import asyncio
 import logging
 import os
+import time
 from typing import Dict, Set
 
 import colorlog
 from git import Repo
 
 import yaml
+from langchain_community.callbacks import get_openai_callback
 
 from ai_code_reviewer.base import ProgrammingPrincipleChecker, ProgrammingPrinciple
 
 
 def get_logger() -> logging.Logger:
-    logger = logging.getLogger()
+    logger = logging.getLogger("ai_code_reviewer")
     logger.setLevel(logging.INFO)
     handler = colorlog.StreamHandler()
     handler.setFormatter(colorlog.ColoredFormatter(
@@ -37,8 +39,7 @@ def get_repo_diff_per_file(repository_path: str, other: str, allowed_extensions:
     return diffs
 
 
-async def review_repo_diff(repo_path: str, compare_with: str):
-    logger = get_logger()
+async def review_repo_diff(repo_path: str, compare_with: str, logger: logging.Logger):
     principles_path = os.path.join(repo_path, PRINCIPLES_DIR)
     if not os.path.isdir(principles_path) or len(os.listdir(principles_path)) == 0:
         logger.error(F"No review principles found. You need to populate '{PRINCIPLES_DIR}' dir with review principles")
@@ -59,14 +60,18 @@ async def review_repo_diff(repo_path: str, compare_with: str):
             review = await programming_principle_checker.review_file_patch(per_file_diff[file_name])
             if not review.approve:
                 logger.warning(f"Review of file {file_name}:\n{review.comments}")
-    logger.info("Review completed")
 
 
 def main():
+    logger = get_logger()
     parser = argparse.ArgumentParser(description="Review code against programming principles.")
     parser.add_argument("--repo_path", type=str, required=False, default=".",
                         help="Path to the repository to review.")
     parser.add_argument("--compare_with", type=str, required=False, default="HEAD",
                         help="Base version to compare with. May be git hash or tag of source branch")
     args = parser.parse_args()
-    asyncio.run(review_repo_diff(args.repo_path, args.compare_with))
+    with get_openai_callback() as cb:
+        start_time = time.time()
+        asyncio.run(review_repo_diff(args.repo_path, args.compare_with, logger))
+        time_spent = time.time() - start_time
+        logger.info(f"Review completed in {round(time_spent, 2)}s and {round(cb.total_cost, 2)}$")
