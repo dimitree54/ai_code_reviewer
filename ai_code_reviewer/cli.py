c26859524dcb46649c21b95bbdfa6bff4909df81
Dmitrii Rashchenko <dimitree54@gmail.com>
2024-02-24 13:03:40 +0200

    update cli script

--- ai_code_reviewer/cli.py
+++ ai_code_reviewer/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import asyncio
+import logging
 import os
 from typing import Dict, Set
 
@@ -8,6 +9,9 @@ from git import Repo
 import yaml
 
 from ai_code_reviewer.base import ProgrammingPrincipleChecker, ProgrammingPrinciple
+
+
+PRINCIPLES_DIR = ".coding_principles"
 
 
 def get_repo_diff_per_file(repository_path: str, other: str, allowed_extensions: Set[str]) -> Dict[str, str]:
@@ -23,7 +27,10 @@ def get_repo_diff_per_file(repository_path: str, other: str, allowed_extensions:
 
 
 async def main(repo_path: str, compare_with: str):
-    principles_path = os.path.join(repo_path, ".coding_principles")
+    principles_path = os.path.join(repo_path, PRINCIPLES_DIR)
+    if not os.path.isdir(principles_path) or len(os.listdir(principles_path)) == 0:
+        logging.error(F"No review principles found. You need to populate '{PRINCIPLES_DIR}' dir with review principles")
+        return
     for principle_file_name in os.listdir(principles_path):
         _, ext = os.path.splitext(principle_file_name)
         if ext != ".yaml":
@@ -38,13 +45,14 @@ async def main(repo_path: str, compare_with: str):
         per_file_diff = get_repo_diff_per_file(repo_path, compare_with, allowed_extensions={".py"})
         for file_name in per_file_diff:
             review = await programming_principle_checker.review_file_patch(per_file_diff[file_name])
-            print(f"review of file {file_name}:")
-            print(review.comments)
+            if not review.approve:
+                logging.warning(f"Review of file {file_name}:\n{review.comments}")
+    logging.info("Review completed")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Review code against programming principles.")
-    parser.add_argument("--repo_path", type=str, required=True,
+    parser.add_argument("--repo_path", type=str, required=False, default=".",
                         help="Path to the repository to review.")
     parser.add_argument("--compare_with", type=str, required=False, default="HEAD",
                         help="Base version to compare with. May be git hash or tag of source branch")
