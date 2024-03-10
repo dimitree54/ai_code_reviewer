b1d00a3de4ca3bbb8bb3d228eadb8d492c455e58
Dmitrii Rashchenko <dimitree54@gmail.com>
2024-03-10 11:11:31 +0200

    test cli with real review

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,4 +1,4 @@
-import subprocess
+import os
 import subprocess
 import unittest
 from pathlib import Path
@@ -75,12 +75,27 @@ class TestCLI(unittest.TestCase):
     def setUp(self):
         self.repo_path = Path(__file__).parents[1]
         self.cli_path = self.repo_path / "ai_code_reviewer" / "cli.py"
+        mock_diff_path = Path(__file__).parent / "data" / "mock_diff.txt"
+        with open(mock_diff_path) as f:
+            self.test_diff = f.read()
 
-    def test_cli_default_empty(self):
+    def test_cli_default_no_diff(self):
         result = subprocess.run([
             'python', str(self.cli_path),
             '--repo_path', str(self.repo_path),
         ], capture_output=True, text=True)
-        self.assertIn("Review completed", result.stderr)
-        self.assertIn("0.00$", result.stderr)
+        self.assertIn("No diff with HEAD.", result.stderr)
 
+    def test_cli(self):
+        fake_repo_file_path = self.repo_path / "fake_repo_file.py"
+        with open(fake_repo_file_path, "w") as fake_repo_file:
+            fake_repo_file.write(self.test_diff)
+        result = subprocess.run([
+            'python', str(self.cli_path),
+            '--repo_path', str(self.repo_path),
+            '--openai_model_name', "gpt-3.5-turbo",
+            '--custom_principles_path', str(self.repo_path / ".coding_principles"),
+            '--file_extensions_to_review', ".py", ".md"
+        ], capture_output=True, text=True)
+        self.assertIn("No diff with HEAD.", result.stderr)
+        os.remove(fake_repo_file_path)
